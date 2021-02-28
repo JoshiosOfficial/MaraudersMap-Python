c3ab3911d849f14ecf0ded5be06efaa20ac939dc
Joshua <joshuafranco1313@gmail.com>
2021-02-28 13:54:31 -0500

    Spelled spells wrong oops

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ client.x_emoji = ':x:'
 client.check_emoji = ':white_check_mark:'
 
 spells_response = await client.session.get(f"https://api.potterworldmc.com/spells")
-spells = await seplls_response.json()
+spells = await spells_response.json()
 
 @client.event
 async def on_ready():
