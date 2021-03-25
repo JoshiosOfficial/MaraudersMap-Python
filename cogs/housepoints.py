9ab4f2087c091c6e28307a99c2cc3e3c13fa1ce2
Joshua <joshuafranco1313@gmail.com>
2021-03-25 13:04:45 -0400

    Change to emotes oops

--- cogs/housepoints.py
+++ cogs/housepoints.py
@@ -22,7 +22,7 @@ class Housepoints(commands.Cog):
                 f"{self.client.emotes[housepoints[1][0].upper()]} {housepoints[1][0].capitalize()} - **{housepoints[1][1]}**\n"
                 f"{self.client.emotes[housepoints[2][0].upper()]} {housepoints[2][0].capitalize()} - **{housepoints[2][1]}**\n"
                 f"{self.client.emotes[housepoints[3][0].upper()]} {housepoints[3][0].capitalize()} - **{housepoints[3][1]}**\n\n"
-                f"{self.client.emotes[housepoints[0][0].upper()]} {housepoints[0][0].capitalize()} is **{housepoints[0][1] - housepoints[1][1]}** points ahead of {self.client.house_emojis[housepoints[1][0]]} {housepoints[1][0].capitalize()}!"
+                f"{self.client.emotes[housepoints[0][0].upper()]} {housepoints[0][0].capitalize()} is **{housepoints[0][1] - housepoints[1][1]}** points ahead of {self.client.emotes[housepoints[1][0].upper()]} {housepoints[1][0].capitalize()}!"
             ),
             color = self.client.house_colors[housepoints[0][0]],
             title = "Current House Points"
